dcd238e6e97fadd744873c24186922c4a046318c
OpenTheDoor <tkddnwkdb@gmail.com>
2023-03-09 23:51:20 +0900

    refactor: Change method name and Add loginUser method, createSuccessMsgFromUserInfo method, createResponseBody method

--- src/main/java/com/space/haramspring/user/service/UserService.java
+++ src/main/java/com/space/haramspring/user/service/UserService.java
@@ -1,34 +1,48 @@
 package com.space.haramspring.user.service;
 
-import com.space.haramspring.core.entity.users.User;
+import com.space.haramspring.user.dto.UserSignupDto;
+import com.space.haramspring.user.dto.UserLoginDto;
+
+import java.util.Map;
 
 public interface UserService {
 
     /**
-     * 사용자의 정보를 메모리 데이터 베이스에서 불러옵니다.
-     * @param userId 사용자 아이디 정보를 받습니다.
-     * @return 사용자 정보 객체를 반환합니다.
+     * getUserInfo - Service
+     * user 정보를 조회합니다. 객체 전부를 받지 않고, 유저가 직접 확인할 수 있는 정보만 제공한다.
+     * Key (userSeqNum) : Value (User) 로 저장되어 있는 store Map 을 이용한다.
+     * @param userId 사용자의 로그인 아이디를 통해 사용자 정보를 조회한다.
+     * @return 사용자 정보를 반환한다.
      */
-    User getUserInfoByRepository(String userId);
+    Map<String, String> getUserInfo(String userId);
 
     /**
-     * 사용자의 정보를 메모리 데이터 페이스에 저장합니다.
-     * @param user 사용자 객체를 받습니다.
-     * @return 사용자 정보 객체를 반환합니다.
+     * addUser - Service ㅣ
+     * 사용자의 정보를 메모리 데이터 베이스에 저장한다.
+     *
+     * @param user 사용자 객체를 받는다.
      */
-    User addUserByRepository(User user);
+    Map<String, String> addUser(UserSignupDto user);
+
+//    /**
+//     * 사용자의 정보를 데이터 베이스에서 수정합니다.
+//     * @param user 변경되는 사용자 객체를 받습니다.
+//     * @return 변경된 객체를 반환합니다.
+//     */
+//    Map<String, String> updateUser(UserSignupDto user);
+
+//    /**
+//     * 사용자의 정보를 데이터 베이스에서 삭제합니다.
+//     * @param userId 삭제할 유저 정보의 아이디를 받습니다.
+//     * @return 삭제 결과 값을 true or false 로 반환합니다.
+//     */
+//    Map<String, String> deleteUser(String userId);
 
     /**
-     * 사용자의 정보를 데이터 베이스에서 수정합니다.
-     * @param user 변경되는 사용자 객체를 받습니다.
-     * @return 변경된 객체를 반환합니다.
+     * loginUser - Service ㅣ
+     * @param userLoginDto 받아온 로그인 데이터를 통해 인증을 실행하고,
+     * @return accessToken 값을 map 으로 제공한다.
      */
-    User updateUserByRepository(User user);
+    Map<String, String> loginUser(UserLoginDto userLoginDto);
 
-    /**
-     * 사용자의 정보를 데이터 베이스에서 삭제합니다.
-     * @param userId 삭제할 유저 정보의 아이디를 받습니다.
-     * @return 삭제 결과 값을 true or false 로 반환합니다.
-     */
-    Boolean deleteUserByRepository(String userId);
 }
--- src/main/java/com/space/haramspring/user/service/UserServiceImpl.java
+++ src/main/java/com/space/haramspring/user/service/UserServiceImpl.java
@@ -1,10 +1,20 @@
 package com.space.haramspring.user.service;
 
 import com.space.haramspring.core.entity.users.User;
+import com.space.haramspring.jwt.JwtUtils;
+import com.space.haramspring.user.dto.UserJwtDto;
+import com.space.haramspring.user.dto.UserSignupDto;
+import com.space.haramspring.user.dto.UserLoginDto;
 import com.space.haramspring.user.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -12,36 +22,152 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final JwtUtils jwtUtils;
 
+    /**
+     * getUserInfo - Service ㅣ
+     * @param userId 사용자의 아이디를 받는다.
+     * @return 해당 아이디로 userSeqNum 을 찾아 저장소에서 회원 정보를 map 형태로 반환한다.
+     */
     @Override
-    public User getUserInfoByRepository(String userId) {
+    public Map<String, String> getUserInfo(String userId) {
 
-        log.info("[UserServiceImpl - getUserInfoByRepository] 레포지토리에게 {} 정보 요청", userId);
-        User user = userRepository.getUserInfoByUserId(userId);
-        return user;
+        Long userSeqNum = userRepository.getSeqNumFindById(userId);
+
+        if (userSeqNum.equals(-1L)) {
+            // 유저가 id를 "fail"로 요청했을 때, 예외처리 필요(다른 형식 사용 요망)
+            return createResponseBody("read", "fail");
+        }
+
+        Optional<User> optionalUser = userRepository.findByUserSeq(userSeqNum);
+
+        return createSuccessMsgFromUserInfo(optionalUser.get());
     }
 
+    /**
+     * addUser - Service ㅣ
+     * 회원가입을 위한 정보를 저장한다.
+     * @param userSignupDto 데이터 베이스에 저장될 정보를 해당 객체로 받아온다.
+     *                      같은 아이디로 저장되어 있는 유저가 있는지 체크한 후,
+     *                      회원 정보를 데이터 베이스에 저장한다.
+     * @return 저장된 회원 정보를 반환한다.
+     */
     @Override
-    public User addUserByRepository(User user) {
+    public Map<String, String> addUser(UserSignupDto userSignupDto) {
 
-        log.info("[UserServiceImpl - addUserByRepository] 레포지토리에게 유저 정보 저장 요청 {}", user);
-        User saveUser = userRepository.addUserByUserObject(user);
-        return saveUser;
+        String userId = userSignupDto.getUserId();
+        log.info("[service] - 유저 추가 로직 실행. 받아온 유저의 아이디는 {}", userId);
+        Long userSeqNum = userRepository.getSeqNumFindById(userId);
+
+        if (!userSeqNum.equals(-1L)) {
+            // 유저가 id를 "fail"로 요청했을 때, 예외처리 필요(다른 형식 사용 요망)
+            return createResponseBody("create", "fail");
+        }
+
+        User user = userRepository.saveUser(userSignupDto);
+
+        return createSuccessMsgFromUserInfo(user);
+
     }
 
-    @Override
-    public User updateUserByRepository(User user) {
 
-        log.info("[UserServiceImpl - updateUserByRepository] 레포지토리에게 유저 정보 변경 요청 {}", user);
-        User newUser = userRepository.updateUserByUserObject(user);
-        return newUser;
+//    @Override
+//    public Map<String, String> updateUser(UserSignupDto user) {
+//
+//        String userId = user.getId();
+//        Long userSeqNum = userRepository.getSeqNumFindById(userId);
+//
+//        if (userSeqNum.equals(-1L)) {
+//
+//            return createResponseBody("update", "fail");
+//        }
+//
+//        UserSignupDto newUser = userRepository.updateUser(userSeqNum, user);
+//
+//        return createResponseBody("update", "success");
+//    }
+
+//    @Override
+//    public Map<String, String> deleteUser(String userId) {
+//
+//        Long userSeqNum = userRepository.getSeqNumFindById(userId);
+//
+//        if (userSeqNum.equals(-1L)) {
+//            return createResponseBody("delete", "fail");
+//        }
+//
+//        userRepository.removeUser(userSeqNum, userId);
+//        return createResponseBody("delete", "success");
+//    }
+
+    /**
+     * loginUser - Service ㅣ
+     * @param userLoginDto 유저가 로그인 요청시 입력한 아이디 패스워드를 불러온다.
+     *                     이를 통해 userSeqNum 을 찾아 유저 정보를 UserJwtDto 에 담아 토큰을 생성하는
+     *                     generate 메서드에게 호출하며 이 정보를 넘긴다.
+     *                     받아온 해쉬 값의 로그인 시간을 loginDate 에 저장한다.
+     * @return 토큰 값을 Map 에 담아 반환한다.
+     */
+    @Override
+    public Map<String, String> loginUser(UserLoginDto userLoginDto) {
+
+        Long userSeqNum = userRepository.getSeqNumFindById(userLoginDto.getUserId());
+        log.debug("[UserServiceImpl] - userSeqNum : {}", userSeqNum);
+
+        if (userSeqNum.equals(-1L)) {
+            return createResponseBody("login", "fail");
+        }
+
+        Optional<User> optionalUser = userRepository.findByUserSeq(userSeqNum);
+
+        User user = optionalUser.get();
+
+        UserJwtDto userJwtDto = UserJwtDto.builder()
+                .userId(user.getUserId())
+                .build();
+
+        String token = jwtUtils.generateToken(userJwtDto);
+
+        log.debug("[UserServiceImpl] - token : {}", token);
+
+        Map<String, String> accessToken = new HashMap<>();
+        accessToken.put("accessToken", "Bearer " + token);
+
+        user.setLoginDate(jwtUtils.extractAllClaims(token).getIssuedAt().toString());
+
+        log.debug("[UserServiceImpl] - User Login Complete: {}", "OK");
+        return accessToken;
     }
 
-    @Override
-    public Boolean deleteUserByRepository(String userId) {
+    /**
+     * createSuccessMsgFromUserInfo - Service ㅣ
+     * 성공 메세지를 만들기 위한 메서드이다.
+     * @param user 유저 객체를 불러온다.
+     * @return 성공 메세지(유저 정보)를 Map 형태로 반환한다.
+     */
+    private static Map<String, String> createSuccessMsgFromUserInfo(User user) {
+        Map<String, String> successMsg = new HashMap<>();
 
-        log.info("[UserServiceImpl - deleteUserByRepository] 레포지토리에게 {} 유저 정보 삭제 요청 ", userId);
-        Boolean isSuccess = userRepository.deleteUserByUserId(userId);
-        return isSuccess;
+        successMsg.put("joinDate", user.getJoinDate());
+        successMsg.put("nickname", user.getNickname());
+        successMsg.put("email", user.getEmail());
+        successMsg.put("userId", user.getUserId());
+
+        return successMsg;
+    }
+
+    /**
+     * createResponseBody - Service ㅣ
+     * 성공 메세지를 반환하기 위한 메서드이다.
+     * @param key 해당 키 값으로
+     * @param value 밸류를 넣어
+     * @return 맵 형태로 반환한다.
+     * @param <T> 어떤 타입으로 반환할지 모르기에 타입 매개변수를 사용했다.
+     */
+    private static <T> Map<String, T> createResponseBody(String key, T value) {
+        Map<String, T> response;
+        response = new HashMap<>();
+        response.put(key, value);
+        return response;
     }
 }
